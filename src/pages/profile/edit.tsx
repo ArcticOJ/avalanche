a399d82019002c7a5abe2942d18a8c0b26600a20
Nguyen Thanh Quang <void.dev@outlook.com>
2023-03-05 17:25:43 +0700

    :arrow_up: bump some dependencies

--- src/pages/profile/edit.tsx
+++ src/pages/profile/edit.tsx
@@ -11,8 +11,11 @@ import {
 import {ChevronDown, ChevronUp, Clipboard, Key, RefreshCw} from 'react-feather';
 import TextBox from 'components/TextBox';
 import {notify} from 'lib/notifications';
+import useFetch from 'lib/hooks/useFetch';
+import {request} from 'lib/utils';
 
 export default function ProfileEdit() {
+  const {data, mutate} = useFetch('/api/user/apiKey');
   return (
     <Flex m={4} gap={2}>
       <NumberInput size='sm' focusBorderColor='arctic.300'>
@@ -27,15 +30,22 @@ export default function ProfileEdit() {
         </NumberInputStepper>
       </NumberInput>
       <TextBox placeholder='API key' icon={Key} isReadOnly>
-        Test
+        {data ? data.apiKey : ''}
       </TextBox>
       <ButtonGroup isAttached>
         <Button leftIcon={<Clipboard size={16} />}
           onClick={() => notify('Hello World', <Button onClick={() => alert('uwu')} />)}>
           Copy
         </Button>
-        <Button rightIcon={<RefreshCw size={16} />}>
-          Reset
+        <Button rightIcon={<RefreshCw size={16} />} onClick={() => {
+          request({
+            endpoint: '/api/user/apiKey',
+            method: 'PATCH'
+          }).finally(() => mutate(null, {
+            rollbackOnError: false
+          }));
+        }}>
+          Regenerate
         </Button>
       </ButtonGroup>
     </Flex>
